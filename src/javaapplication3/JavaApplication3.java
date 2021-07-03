7310c2d50fd9998450372aaa0c960bc1795cd495
Jacek Podkanski <ruby.object@googlemail.com>
2021-07-03 18:35:46 +0100

    got rid of deps.edn clojure

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -17,17 +17,17 @@ public class JavaApplication3 {
      */
     public static void main(String[] args) {
         System.out.println("the beginning");
-        
+        /*
         Helloer helloer = new Helloer();
         
         helloer.everyone();
         
         String rs = helloer.everyoneReturned();
         System.out.println(rs);
+        */
+        //jac.responder.foo("baar");
         
-        jac.responder.foo("baar");
-        
-        jac.responder.wow(helloer);
+        //jac.responder.wow(helloer);
         
         System.out.println("the beginning");
     }
