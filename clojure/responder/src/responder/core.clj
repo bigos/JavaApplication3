be01bfabbb99491f9f2af47bd21a8dc1e550b764
Jacek Podkanski <ruby.object@googlemail.com>
2021-07-03 19:11:38 +0100

    copied old code to be used by java

--- clojure/responder/src/responder/core.clj
+++ clojure/responder/src/responder/core.clj
@@ -1,6 +1,15 @@
-(ns responder.core)
+(ns responder.core
+  (:gen-class
+   :methods [#^{:static true} [wow [Helloer] void]  ;we have a problem
+             #^{:static true} [foo [String] void]]))
 
-(defn foo
+(defn -wow
+  "experiment with passing more elaborate objects from java"
+  [x]
+  (prn "this is the type of the argument passed")
+  (prn (type x)))
+
+(defn -foo
   "I don't do a whole lot."
   [x]
-  (println x "Hello, World!"))
+  (println x "Hello, from clojure!"))
