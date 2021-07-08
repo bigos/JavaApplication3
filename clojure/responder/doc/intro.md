22d4c241e6c6b08762428f64618ed57b02536875
Jacek Podkanski <ruby.object@googlemail.com>
2021-07-08 18:30:39 +0100

    removed clojure project and the jar file

--- clojure/responder/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to responder
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- clojure/responder/test/jac/responder_test.clj
+++
@@ -1,7 +0,0 @@
-(ns jac.responder-test
-  (:require [clojure.test :refer :all]
-            [jac.responder :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
